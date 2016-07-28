02fd76e5d7f9bc66005b101aeee5bd42ec00c44e
JFEsquivel <JFEsquivel@LREFUSUARIO>
2016-07-28 11:26:10 -0500

    Intranet LOGIN

---
+++ app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'PagesController@home')->name('home');
+Route::get('proyectos/', 'PagesController@proyectos')->name('proyectos');
+
+//Autenticación
+Route::get('auth/login', [
+    'as' => 'auth.login',
+    'uses' => 'Auth\AuthController@getLogin'
+]);
+
+Route::post('auth/login', [
+    'as' => 'auth.login',
+    'uses' => 'Auth\AuthController@postLogin'
+]);
+
+Route::get('auth/logout', [
+    'as' => 'auth.logout',
+    'uses' => 'Auth\AuthController@getLogout'
+]);
